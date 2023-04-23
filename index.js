d8052b60956523b03393d2396fcd4263d035dd07
Vasily <vasilyserediuk@gmail.com>
2023-04-23 23:32:53 +0100

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+
+const axios = require('axios');
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const router = express.Router();
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIO(server, {
+  cors: {
+    origin: '*',
+  }});
+
+const PORT = process.env.PORT || 3001;
+
+let frameSrc = 'https://www.example.com'; // адрес фрейма по умолчанию
+let frameTime = 0; // текущее время фрейма
+
+// обработчик события подключения нового клиента
+io.on('connection', (socket) => {
+  // отправляем текущий адрес фрейма новому клиенту
+  socket.emit('frameSrcChanged', frameSrc);
+
+  // обработчик события изменения адреса фрейма
+  socket.on('frameSrcChanged', async newFrameSrc => {
+    frameSrc = newFrameSrc;
+    io.emit('frameSrcChanged', frameSrc);
+  });
+
+  // обработчик события загрузки фрейма на клиенте
+  socket.on('frameLoaded', () => {
+    io.emit('frameLoaded'); // оповещаем всех клиентов о загрузке фрейма
+  });
+
+  // обработчик события получения времени фрейма от клиента
+  socket.on('frameTime', (time) => {
+    frameTime = time;
+    io.emit('frameTime', frameTime); // отправляем текущее время фрейма всем клиентам
+  });
+});
+
+router.get('/iframe', (req, res) => {
+  const url = req.query.url;
+  axios.get(url)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Ошибка получения данных');
+    });
+});
+
+app.listen(PORT, function () {
+  console.log('CORS-enabled web server listening on port ' + PORT);
+});
